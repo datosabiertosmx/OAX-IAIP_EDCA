5f062c32d7c99b1f6419ad7a8b5bf1631ccef2eb
Contrataciones-Abiertas <contratacionesabiertas@inai.org.mx>
2021-03-01 20:21:37 -0600

    Actualización versión IAIP Oaxaca
    Footer con datos propios de IAIP Oaxaca
    Header con el logo de IAIP Oaxaca
    Breadcum nombre del IAIP Oaxaca

--- contratacionesabiertas/captura/db_conf.js
+++ contratacionesabiertas/captura/db_conf.js
@@ -6,7 +6,7 @@ var pgp = require('pg-promise')(options);
 const config = {
     host: process.env.POSTGRES_HOST || 'localhost',
     port: process.env.POSTGRES_PORT || 5432,
-    database: process.env.POSTGRES_NAME ||'edca_generica',
+    database: process.env.POSTGRES_NAME ||'edca',
     user: process.env.POSTGRES_USER || 'prueba_captura',
     password: process.env.POSTGRES_PASSWORD || 'p4ssw0rd'
 };
@@ -18,17 +18,17 @@ console.log('DB Config -> ', JSON.stringify(config));
 const configDash = {
     host: process.env.POSTGRES_HOST || 'localhost',
     port: process.env.POSTGRES_PORT || 5432,
-    database: process.env.POSTGRES_NAME ||'edca_generica',
+    database: process.env.POSTGRES_NAME ||'edca',
     user: process.env.POSTGRES_USER || 'prueba_dashboard',
     password: process.env.POSTGRES_PASSWORD || 'p4ssw0rd'
 };
 
 const globals = {
     site : {
-        port : 3020
+        port : 3000
     },
     node: {
-        port : 6020
+        port : 6000
     }
 }
 
--- contratacionesabiertas/dashboard/dash_config.js
+++ contratacionesabiertas/dashboard/dash_config.js
@@ -6,14 +6,14 @@ var pgp = require('pg-promise')(options);
 const configDash = {
     host: process.env.POSTGRES_HOST || 'localhost',
     port: process.env.POSTGRES_PORT || 5432,
-    database: process.env.POSTGRES_NAME ||'edca_generica',
+    database: process.env.POSTGRES_NAME ||'edca',
     user: process.env.POSTGRES_USER || 'prueba_dashboard',
     password: process.env.POSTGRES_PASSWORD || 'p4ssw0rd'
 };
 
 const globals = {
     site : {
-        port : 4020
+        port : 4000
     }
 }
 
